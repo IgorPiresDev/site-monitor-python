69e43a761cb2706ace96db6d6b2c82f7b04f5902
unknown <igorpiris@hotmail.com>
2026-02-14 21:06:34 -0300

    feat: monitor com alertas coloridos, logs em JSON e README

---
+++ README.md
@@ -0,0 +1,12 @@
+🧠 Aprendizados:
+Durante o desenvolvimento, superei desafios como:
+
+Manipulação de tipos: Corrigir erros de serialização JSON (conflitos entre a biblioteca time e variáveis de string).
+
+Escopo e Identação: Organizar blocos try/except para garantir que o log fosse salvo mesmo em caso de erro de conexão.
+
+Controle de Versão: Uso de branches para testes de funcionalidades antes de integrá-las à versão estável (master).
+
+Desenvolvido por Igor Aparecido Marque Pires 🎓
+
+Estudante de Ciência da Computação - UNINTER
--- monitor_devOps.py
+++ monitor_devOps.py
@@ -1,25 +1,50 @@
 import requests as rq
 import time
 import datetime
+import json
 
+headers = {
+    'User-Agent': 'Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+}
 
 
 http_rq= ['https://www.youtube.com/','https://www.google.com','https://www.uninter.com','https://gemini.google.com/']
 
 
 while True:
-    try:
-        time_bz= datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        with open('log_monitor.txt', 'a') as arquivo:
+    
+    for link in http_rq:
+        time_data= datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        
+        try:
 
-            for link in http_rq:
-                req = rq.get(link)
-            
+            req = rq.get(link, headers=headers, timeout=10)
+            status= req.status_code
+        except Exception as e:      
+            status = 'offline'
+        
+        VERDE = '\033[92m'
+        AMARELO = '\033[93m'
+        VERMELHO = '\033[91m'
+        RESET = '\033[0m'
 
-                if req.status_code == 200:
-                    arquivo.write(f'CHECK TIME: {time_bz}, {link} ONLINE\n')
-                else:
-                    arquivo.write(f'CHECK TIME: {time_bz}, {link} OFFLINE\n')
+        if status == 200:
+            print(f"{VERDE} [ONLINE] {link} -> {status}{RESET}")
+        elif status == 403:
+            print(f"{AMARELO}  [BLOQUEADO] {link} -> {status} (Servidor recusou o robô){RESET}")
+        else:
+            print(f"{VERMELHO}🚨 [OFFLINE/ERRO] {link} -> {status}{RESET}")
 
-    except:'error critico!'
+        log_data= {
+                'timestamp': time_data,
+                'url': link,
+                'status': status
+        }
+
+        print(f'[{time_data}]{link} -> {status}')
+
+        with open ('logs.json','a') as f:
+            f.write(json.dumps(log_data)+'\n')
+
+    print("-" * 30) 
     time.sleep(10)
